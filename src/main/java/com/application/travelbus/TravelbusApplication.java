41933af8515b9b9ac8ebb675a8c59951d0b73475
abdillahadam20 <adamabdillah20@gmail.com>
2021-12-03 10:13:16 +0700

    first commit

---
+++ src/main/java/com/application/travelbus/TravelbusApplication.java
@@ -0,0 +1,13 @@
+package com.application.travelbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelbusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelbusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/travelbus/TravelbusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.travelbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelbusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
